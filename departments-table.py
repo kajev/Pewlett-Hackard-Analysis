bd989fd2a12690f11629d7e469f4abef80a92c0d
kajev <kajevm@gmail.com>
2021-12-19 14:16:42 -0500

    adding retirement_info.csv

--- departments-table.py
+++ departments-table.py
@@ -5,3 +5,48 @@ CREATE TABLE departments (
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+CREATE TABLE employees (
+	emp_no INT NOT NULL,
+	bith_data VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES titles (emp_no),
+	PRIMARY KEY(emp_no)
+);
+CREATE TABLE dept_manager (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+  	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  	PRIMARY KEY (emp_no)
+);
+CREATE TABLE dept_emp (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date VARCHAR NOT NULL,
+	to_date VARCHAR NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY(dept_no, emp_no)
+);
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR  NOT NULL,
+	from_date VARCHAR NOT NULL,
+	to_date VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
+	PRIMARY KEY (emp_no)
+);
+SELECT * FROM departments;
