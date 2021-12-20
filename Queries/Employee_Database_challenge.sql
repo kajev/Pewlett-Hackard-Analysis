1977463c69ec7c7d2340b549750e4dcd2a891d18
kajev <kajevm@gmail.com>
2021-12-19 22:58:45 -0500

    finished deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
-	bith_data VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE dept_manager (
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-	--FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
@@ -38,7 +38,7 @@ CREATE TABLE dept_emp (
 	from_date VARCHAR NOT NULL,
 	to_date VARCHAR NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	--FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY(dept_no, emp_no)
 );
 CREATE TABLE titles (
@@ -49,3 +49,45 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
 	PRIMARY KEY (emp_no, title, from_date)
 );
+
+
+--DELIVERABLE 1
+SELECT  e.emp_no, e.first_name, e.last_name,
+		t.title, t.from_date, t.to_date
+--INTO retirement_titles
+FROM employees as e
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (e.bith_data BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, title DESC;
+
+
+SELECT COUNT(ut.emp_no), ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+--DELIVERABLE 2
+SELECT DISTINCT ON(e.emp_no)e.emp_no, e.first_name, e.last_name, e.bith_data, 
+		de.from_date, de.to_date, t.title
+--INTO mentorship_eligibility
+FROM employees as e
+	LEFT OUTER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	LEFT OUTER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01' AND e.bith_data BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no
+
